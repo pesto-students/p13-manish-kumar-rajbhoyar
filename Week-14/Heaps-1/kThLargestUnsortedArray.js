12551186d1919a9a66bdd457c04e4a23fbd15f9e
Raj Bhoyar <Rajbhoyar21@gmail.com>
2023-12-15 14:48:39 +0530

    Week-14

---
+++ Week-14/Heaps-1/kThLargestUnsortedArray.js
@@ -0,0 +1,95 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+    push(val) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+    pop() {
+        const max = this.heap[0];
+        const end = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = end;
+            this.bubbleDown();
+        }
+        return max;
+    }
+    peek() {
+        return this.heap[0];
+    }
+    bubbleUp() {
+        let idx = this.heap.length - 1;
+        const element = this.heap[idx];
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.heap[parentIdx];
+            if (element >= parent) break;
+            this.heap[parentIdx] = element;
+            this.heap[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+    bubbleDown() {
+        let idx = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+            if (leftChildIdx < length) {
+                leftChild = this.heap[leftChildIdx];
+                if (leftChild < element) {
+                    swap = leftChildIdx;
+                }
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.heap[rightChildIdx];
+                if (
+                    (swap === null && rightChild < element) || 
+                    (swap !== null && rightChild < leftChild)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if (swap === null) break;
+            this.heap[idx] = this.heap[swap];
+            this.heap[swap] = element;
+            idx = swap;
+        }
+    }
+}
+
+
+var findKthLargest = function(nums, k) {
+    let heap = new MinHeap();
+
+    // Create a heap with the first k elements of nums. Transform this list into a min-heap.
+    for (let i = 0; i < k; i++) {
+        heap.push(nums[i]);
+    }
+
+    /* 
+    For each of the remaining elements in nums:
+    If the element is larger than the smallest element in the heap (i.e., the top of the heap):
+    Remove the top element from the heap.
+    Insert the current element into the heap.
+    */
+
+    for (let i = k; i < nums.length; i++) {
+        if (nums[i] > heap.peek()) {
+            heap.pop();
+            heap.push(nums[i]);
+        }
+    }
+    // After processing all elements in nums, the top of the heap will contain the kth largest element.
+    return heap.peek();
+};
+
+const input = prompt('Enter input: ');
+const array = input.split(',');
+const k = prompt('Enter k: ');
+const result = findKthLargest(array, k);
+console.log(result);
